e819e517b76a98399227ab8c471b5de2c68c8af5
sharrm <90279990+sharrm@users.noreply.github.com>
2021-10-17 13:41:20 -0700

    [update] Add updated week 3 assignment

--- Week_3/CE640_HW3_sharrm.m
+++ Week_3/CE640_HW3_sharrm.m
@@ -1,11 +1,24 @@
-% Week 3 assignment description
+% CE640 Week 3 - Fall 2021
+% Matt Sharr (sharrm)
+
+% Week 3 assignment to compute the sum of multiple Gaussian profiles.
+
+% Using this script, a user can customize their vector (x) and matrix (A) 
+% input values and compute normal distribution curves for any number of 
+% amplitudes, standard deviations, and means.
+
+% Accompanied by this script is a function called 'multGauss_sharrm' which 
+% is where the main calculation and plotting for each Gaussian profile is 
+% done. The maximum number of columns for matrix 'A' can only be three to 
+% use this function.
 
 x = 0:0.1:20; % Vector to pass into multGauss
-A = [0.1,0.5,2;0.5,1,4;1,2,6;2,3,8]; % Matrix containing amplitude, width (stdev), and location (mean) of Gaussian profiles
+A = [0.1,0.5,2;0.5,1,4;1,2,6;2,3,8]; % Matrix containing amplitude, width 
+%(stdev), and location (mean) of Gaussian profiles
 
 [rows, cols] = size(A); % Determine number the dimensions of the input matrix
 
-if cols==3 % Check that the number of columns is 3
+if cols==3 % Check that the number of columns in matrix 'A' is 3
      multGauss_sharrm(x, A, rows) % Enter multGauss function
 elseif cols~=3 % If the number of columns is not 3, inform the user and exit the script
     explanation = 'Please check input array size. The number of columns should be 3, not ';
--- Week_3/multGauss_sharrm.m
+++ Week_3/multGauss_sharrm.m
@@ -1,37 +1,46 @@
-function mg=multGauss(vector, matrix, rows)
-%multGauss 
-%   
-% x will be in the main script
+function mg=multGauss(vector, matrix, rows) % function accepting three inputs
+% CE640 Week 3 - Fall 2021
+% Matt Sharr (sharrm)
 
-% the function will be in this part here
-% function needs to be general, accept a matrix, and the x vector
+y = 0; % Establish function 'y' as zero to be used in our For loop
+x = vector; % Define 'x' as the user provided vector
 
-y = 0; % Define y as zero to begin
+disp('Computing Gaussian profiles using the input parameters...')
 
+% source: https://www.mathworks.com/help/matlab/ref/hold.html
 hold on % Add a new plot without deleting the existing
 
-for i = 1:rows % For all of the rows in the input matrix, from 1 to our known number of rows
+% Iterate through all rows in the input matrix. Compute Gaussian profiles
+% for each row, and sum all rows as a single Gaussian profile. Plot each
+% profile.
+for i = 1:rows
+    % Establish user readable variables for each Gaussian input parameter
+    % (amplitude, standard deviation, and position)
     amp = matrix(i,1); % Amplitude portion of the input matrix
     stdev = matrix(i,2); % Standard Deviation portion of the input matrix
     pos = matrix(i,3); % Position (mean) portion of the input matrix
     
-    y_i = amp*gaussmf(vector, [stdev pos]); % Use Matlabs gaussmf function
-    y = y + y_i; % Save the current interation of gaussmf and add to the previous
+    % source: https://www.mathworks.com/help/fuzzy/gaussmf.html
+    y_i = amp*gaussmf(x, [stdev pos]); % Use Matlab's gaussmf function
+    
+    y = y + y_i; % Add the current interation of gaussmf to the previous
      
-    plot(vector, y_i) % Add gaussmf calculation to plot
+    plot(x, y_i) % Add the current gaussmf calculation to the plot
 end
 
-plot(vector, y, 'g') % Add summation of all 'y' calculation to plot
+plot(x, y, 'g') % Add summation of all 'y' functions to plot
 
 %% Add title, axis labels, and legend to the plot
-title('Gaussian Normal Distribution Profiles')
+title('Gaussian (Normal Distribution) Profiles')
 xlabel('x')
-ylabel('f(x;σ,c)=e^(−(x−c)^2/2σ^2)')
+ylabel('y(x;σ,c)=e^(−(x−c)^2/2σ^2)')
 
+% Modify the plot legend to associate each y(n) with a specific line color,
+% based on the input matrix row
 for i = 1:rows
     Legend{i} = ['y', num2str(i)] ;
 end
 
-Legend = [Legend 'y'];
-legend(Legend)
+Legend = [Legend 'y']; % Add the final summation y to the legend
+legend(Legend) % Show the legend
     
