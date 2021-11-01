4b64fb8d9e06aac1f9d87208a69b6a5c33c18791
sharrm <90279990+sharrm@users.noreply.github.com>
2021-11-01 16:00:40 -0700

    [update] Updated version with customized plot and annimation

--- Week_5/CE640_Week5_sharrm.m
+++ Week_5/CE640_Week5_sharrm.m
@@ -8,18 +8,19 @@ clear all
 asc_files = dir('*.asc'); % Find all ESRI .asc files in the directory
 num_files = numel(asc_files); % Find the number of .asc files
 
-%% Find size of input files
+%% Find size of input files and pre-allocate array with zeros
 
 [R,H] = arcgridread(asc_files(1).name);
-
 raster_data = zeros(size(R));
+
 
 %% Read all of the .asc files identified above, and store the raster and header
 % information in arrays. Plot the read files in a figure window.
 
-for i=1:num_files
+for i=1:num_files  
     [R,H] = arcgridread(asc_files(i).name);
     raster_data = raster_data + R;
+    A{i} = raster_data;
 end
 
 % Convert from mm to m
@@ -28,27 +29,55 @@ raster_data = raster_data / 1000;
 %% Plotting and plot formatting
 
 % Display the summarized precipition data
-f = figure()
-m = mapshow(raster_data,H, 'DisplayType', 'Surface');
+f = figure(1);
+m = geoshow(raster_data, H, 'displaytype', 'texturemap');
 hold on
 
-% surf(raster_data,H,[],-10)
-
-% Customize plot
-% set(gca,'xticklabel',[], 'yticklabel', [],'XTick',[], 'YTick', []);
+% Customize axis labels, plot colormap, plot size, and colorbar location
 ylabel('Latitude (N)');
 xlabel('Longitude (W)');
 title('Total Annual Precipitation (Oregon 1983)');
-f.Position = [75 75 700 400];
+f.Position = [50 50 700 400];
 cmap = colormap(turbo);
 cmap = flipud(cmap);
 colormap(cmap);
-c = colorbar;
-c.Location = 'eastoutside';
-c.Label.String = 'Precipitation (m)';
+cb = colorbar;
+cb.Location = 'eastoutside';
+cb.Label.String = 'Precipitation (m)';
 
-% Overlay contours
-%  m = mapshow(raster_data,H,'DisplayType','contour','LineColor','k','LevelStep', 0.75, 'ShowText', 'off');
+% Set NaN values to transparent since texturemap assumes NaNs as lowest
+% scale value
+m.AlphaDataMapping = 'none';
+m.FaceAlpha = 'texturemap';
+alpha(m,double(~isnan(raster_data)));
 
-% set(gca,'color','white')
+% Add contours for each m of precipitation
+mapshow(raster_data, H,'DisplayType','contour','LineColor','k','LevelStep', 1, 'ShowText', 'off');
+
+% Save output figure at the same size as screen resolution
+set(gcf,'PaperPositionMode','auto')
+print('OR_1983_Precipitation','-dpng','-r0')
+
+%% Create annimation
+
+%save to file
+w = VideoWriter('precip2.avi');
+% w.FrameRate = 1;
+open(w);
+
+% https://www.geeksforgeeks.org/matlab-convert-video-into-slow-motion/
+
+for j=1:num_files
+    for f=1:100
+        figure(2)
+        hold off
+        mapshow(A{j},H,'DisplayType', 'surface')
+        hold on
+
+        cf = getframe(gcf); % Get current frame
+        writeVideo(w, cf);
+    end
+end
+
+close(w);
 
