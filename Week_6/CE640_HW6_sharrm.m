9e023c62f61424d444d2b85afffff9dc5d0388a0
sharrm <90279990+sharrm@users.noreply.github.com>
2021-11-08 16:33:24 -0800

    [update] Updated formatting

--- Week_6/CE640_HW6_sharrm.m
+++ Week_6/CE640_HW6_sharrm.m
@@ -12,16 +12,45 @@ u = [57.12 75.78 83.77 89.58 94.16 97.99 100.81 102.13 102.62];
 k = 0.4; % von Karman constant
 v = 0.01; % Viscosity of water (cm^2s^-1)
 
-%% Log
+%% Smooth Wall Model
 
-u1 = 5.5; % Best estimated shear velocity
+u1 = 5.5; % Initial estimated shear velocity guess
 
 % Smooth log law
 smooth = @(u1,u) ((u1/k)*log((y*u1)/v));
 [s_beta,R,JCOVB,MSE] = nlinfit(y, u, smooth, [1]);
 
+% Print message to the user
 disp(['1. The best estimated shear velocity for the smooth wall model is: ' num2str(s_beta) ' (m/s)'])
 
+%% Rough Wall Model
+
+u2 = 6.1; % Initial estimated shear velocity guess
+ks = 0.1; % Initial estimated height roughness guess
+
+% Rough log law
+rough = @(u2,u) ((u2/k)*log(y/ks)+u2*8.5);
+[r_beta,R,JCOVB,MSE] = nlinfit(y, u, rough, [1]);
+
+% Print messages to the user
+disp(['2a. The best estimated shear velocity for the rough wall model is: ' num2str(r_beta) ' (m/s)'])
+disp(['2b. The best estimated roughness height for the rough wall model is: ' num2str(MSE) ' (cm)'])
+
+%% Plot the input data vs the computed best fit
+
+figure
+hold on
+scatter(y, u, 'k', 'x');
+plot(y, smooth(s_beta,u1), 'b')
+plot(y, rough(r_beta, u2), 'r')
+axis([0 3.5 50 110])
+hold off
+title('Turbulet Boundary Layer Flow')
+legend('Input Data', 'Smooth Wall Fit', 'Rough Wall Fit', 'Location', 'east')
+xlabel('Depth (cm)')
+ylabel('Velocity (cm/s)')
+
+%% Delete
 % Plot the input data vs the computed best fit
 % f1 = figure;
 % figure(f1)
@@ -34,27 +63,3 @@ disp(['1. The best estimated shear velocity for the smooth wall model is: ' num2
 % legend('Input Data', 'Best Fit', 'Location', 'northwest')
 % xlabel('Depth (cm)')
 % ylabel('Velocity (cm/s)')
-
-%%
-
-u2 = 6.1;
-ks = 0.1;
-
-rough = @(u2,u) ((u2/k)*log(y/ks)+u2*8.5);
-[r_beta,R,JCOVB,MSE] = nlinfit(y, u, rough, [1]);
-
-disp(['2a. The best estimated shear velocity for the rough wall model is: ' num2str(r_beta) ' (m/s)'])
-disp(['2b. The best estimated roughness height for the rough wall model is: ' num2str(MSE) ' (cm)'])
-
-f2 = figure;
-figure(f2)
-hold on
-scatter(y, u, 'k', 'x')
-plot(y, smooth(s_beta,u), 'b')
-plot(y, rough(r_beta, u), 'r')
-axis([0 3.5 50 110])
-hold off
-title('Turbulet Boundary Layer Flow')
-legend('Input Data', 'Smooth Wall Fit', 'Rough Wall Fit', 'Location', 'east')
-xlabel('Depth (cm)')
-ylabel('Velocity (cm/s)')
