a20e955527ae15474f9d2ac82d8aece9a4b4bb65
sharrm <90279990+sharrm@users.noreply.github.com>
2021-11-22 13:03:37 -0800

    [update] Latest update with working filters

--- Week_8/CE640_HW8_sharrm.m
+++ Week_8/CE640_HW8_sharrm.m
@@ -17,53 +17,69 @@ infra_amp = 0.25; % meters
 tides_period = 45000; % seconds
 tides_amp = 1.5; % meters
 
-% Part iv
-noise_period = rand(); % seconds
-noise_amp = 0.01; % meters
-
 %% Functions
 
-t = 0:0.5:250; % 604800 s/week
+t = 0:0.5:604800; % 604800 s/week
 
 % Create a sin wave for each signal present in the ocean
 wave = wave_amp * sin((2 * pi/wave_period) * t);
 infra = infra_amp * sin((2 * pi/infra_period) * t);
 tides = tides_amp * sin((2 * pi/tides_period) * t);
-noise = noise_amp * sin((2 * pi/noise_period) * t);
 
 % Combine all signals into a single wave
-combined = wave + infra + tides + noise;
+combined = wave + infra + tides;
+
+% Add noise to the ocean signal
+combined_noise = combined + (0.1 * (randn(size(combined)) - 0.5));
+
+figure(2)
+
+subplot(4,1,1)
+plot(t,combined_noise)
+title('raw')
+
+subplot(4,1,2)
+plot(t(1:360),wave(1:360))
+title('original wave')
+
+subplot(4,1,3)
+plot(t(1:1600),infra(1:1600))
+title('original infra')
+
+subplot(4,1,4)
+plot(t,tides)
+title('original tides')
 
 %% Filtering
 
 % https://www.mathworks.com/help/signal/ref/butter.html
 
-hold on
-
-subplot(2,2,1)
-plot(t,combined, 'k')
+figure(1)
+subplot(4,1,1)
+plot(t, combined_noise, 'Color', [0.1 0.7 0.9])
 title('Raw Data')
 ylabel('meters'); xlabel('seconds')
 
-subplot(2,2,2)
-[B,A]=butter(5, [0.02 0.08]);
-wave_filt=filtfilt(B,A,wave);
-plot(t,wave_filt,'r')
+subplot(4,1,2)
+[B,A]=butter(1, [0.075 0.12]);
+wave_filt=filtfilt(B,A,combined_noise);
+plot(t(1:360), wave_filt(1:360), 'Color', [0.4 0.4 0.2])
 title('Wave Signal')
 ylabel('meters'); xlabel('seconds')
 
-subplot(2,2,3)
-[B,A]=butter(5, [0.02 0.08]);
-infra_filt=filtfilt(B,A,infra);
-plot(t,infra_filt,'b')
+subplot(4,1,3)
+[D,C]=butter(1, [0.003 0.0075]);
+infra_filt=filtfilt(D,C,combined_noise);
+plot(t(1:1600), infra_filt(1:1600), 'Color', [0.7 0.5 0.9])
 title('Infragravity Signal')
 ylabel('meters'); xlabel('seconds')
 
-subplot(2,2,4)
-[B,A]=butter(5, [0.02 0.08]);
-noise_filt=filtfilt(B,A,noise);
-plot(t,noise_filt,'g')
-title('Noise')
+subplot(4,1,4)
+Wn = 0.0005;
+[F,E]=butter(1, Wn, 'low');
+tides_filt=filtfilt(F,E,combined_noise);
+plot(t, tides_filt, 'Color', [0.8 0.2 0.4])
+title('Tide Signature')
 ylabel('meters'); xlabel('seconds')
 
 sgtitle('Ocean Signal Time Series')
